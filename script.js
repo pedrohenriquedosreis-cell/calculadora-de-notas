b0789bfb2b84a579dc52c6c8561d831a2b735730
pedrohenriquedosreis-cell <pedro.henriquedos.reis@escola.pr.gov.br>
2026-09-25 14:00:44 +0000

    eee

---
+++ script.js
@@ -0,0 +1,32 @@
+function calcularMedia(){
+    const textoNota1 = document.getElementById("nota1").value;
+const textoNota2 = document.getElementById("nota2").value;
+const textoNota3 = document.getElementById("nota3").value;
+
+if (textoNota1 ==="" || textoNota2 ==="" || textoNota3 ===""){
+    alert("Preencha todas as notas!")
+    return
+}
+
+const nota1 = Number(textoNota1);
+const nota2 = Number(textoNota2);
+const nota3 = Number(textoNota3);
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+let situacao;
+
+if (media >= 7){
+    situacao ="APROVADO";
+}
+else if (media >= 5){
+    situacao ="RECUPERAÇÂO";
+} else {
+    situacao = "REPROVADO"
+
+}
+const mediaFormatada = media.toFixed(1)
+
+document.getElementById("resultado"). textContent = 
+          "Media: "+ mediaFormatada + "Situação: "+ situacao;
+}
